69c237dd42e9cc475f9ca6b8b7c644e538cb4c54
zhangtaoqd <zhangtao@penavicogw.com>
2015-06-30 12:40:30 +0800

    2015-06-01 create

--- themes/yuekucrm/footer.php
+++ themes/yuekucrm/footer.php
@@ -1,10 +1,8 @@
 <div class="row" >
     <footer class="footer-main">
-        <div class="col-xs-4 col-xs-offset-4" style="text-align: center;">
-            <ul> 网站备案号：<?php echo get_option('zh_cn_l10n_icp_num');?> </ul>
-
-            <ul>  2015 </ul>
-
+        <div style="text-align: center;">
+            <ul> 网站备案号：<?php echo get_option('zh_cn_l10n_icp_num');?>地址：山东省烟台市开发区珠江路32号
+            </ul>
         </div>
     </footer>
 </div> <!-- row -->
